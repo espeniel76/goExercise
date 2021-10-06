3591c32b9f228f1a697797a8f65689c897bf3b88
espeniel76 <espeniel69@gmail.com>
2021-10-06 18:58:29 +0900

    아아

--- programers/level1/6.소수만들기.go
+++ programers/level1/6.소수만들기.go
@@ -12,10 +12,36 @@ import "fmt"
 
 func Ex06() {
 	fmt.Println(solution6([]int{1, 2, 3, 4}))
+	// i := 0
+	// for i = 1; i <= 1000; i++ {
+	// 	if IsPrimeNumber(i) > 0 {
+	// 		// printf("%3d ", i); //i를 출력
+	// 		fmt.Printf("%3d ", i)
+	// 	}
+	// }
+	// fmt.Println("\n")
+}
+
+// 소수점 구하는 함수
+func IsPrimeNumber(n int) int {
+	i := 0
+	last := n / 2
+	if n <= 1 {
+		return 0
+	}
+	for i = 2; i <= last; i++ {
+		if (n % i) == 0 {
+			return 0
+		}
+	}
+	return 1
 }
 
 func solution6(nums []int) int {
-	answer := -1
+
+	for key, val := range nums {
+
+	}
 
 	return answer
 }
